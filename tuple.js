b60f8a00b8eed378422398c4066577db347097b3
Sakshi Jogdand <sakshijog7774@gmail.com>
2025-07-26 12:44:02 +0530

    add

--- tuple.js
+++ tuple.js
@@ -1,1 +1,6 @@
 "use strict";
+// this is a array 
+// var emp:string[] = ['anil','sam','ram'];
+var emp = ['anil', 30, true];
+emp.push('city');
+console.log(emp);
---
+++ tuple.ts
@@ -0,0 +1,5 @@
+// this is a array 
+// var emp:string[] = ['anil','sam','ram'];
+var emp:[string,number,boolean] = ['anil',30,true];
+emp.push('city');
+console.log(emp);
